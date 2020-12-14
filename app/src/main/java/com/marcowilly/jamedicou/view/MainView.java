a83bb863891bdc26520e58f009b11093179e8b36
Marco Willy <44058572+marcowilly@users.noreply.github.com>
2020-12-13 22:56:27 -0300

    start
    start a deleopment project

---
+++ app/src/main/java/com/marcowilly/jamedicou/view/MainView.java
@@ -0,0 +1,16 @@
+package com.marcowilly.jamedicou.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.marcowilly.jamedicou.R;
+
+public class MainView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_view);
+    }
+}
---
+++ app/src/main/java/com/marcowilly/jamedicou/view/SplashView.java
@@ -0,0 +1,16 @@
+package com.marcowilly.jamedicou.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.marcowilly.jamedicou.R;
+
+public class SplashView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_view);
+    }
+}
